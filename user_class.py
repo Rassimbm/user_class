69eb5408fd7f5533fbac90d65fd36685f63c5625
Rassim Benmhamed <rassimbenmhamed@outlook.com>
2023-10-26 00:02:15 -0500

    added display_user_infos method

--- user_class.py
+++ user_class.py
@@ -7,5 +7,15 @@ class User:
         self.is_rewards_member = False
         self.gold_card_points = 0
 
+    def display_user_infos(self):
+        user_infos = f'''
+        Full name: {user_1.fname} {user_1.lname}
+        Email: {user_1.email}
+        Age: {user_1.age}
+        Membeership tatus: {user_1.is_rewards_member}
+        Rewards card points: {user_1.gold_card_points}
+        '''
+        return user_infos
+
 user_1 = User("Rassim", "Benmhamed", "rassimb@ninja.com", 35)
-print(user_1)
+print(user_1.display_user_infos())
