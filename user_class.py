4c6a47dd8a392439116eac8523f19aed6d952b4d
Rassim Benmhamed <rassimbenmhamed@outlook.com>
2023-10-26 13:09:38 -0500

    updated code: corrected display_user_infos() and applied chaining methods

--- user_class.py
+++ user_class.py
@@ -8,32 +8,29 @@ class User:
         self.gold_card_points = 0
 
     def display_user_infos(self):
-        user_infos = f'''
-        Full name: {user_1.fname} {user_1.lname}
-        Email: {user_1.email}
-        Age: {user_1.age}
-        Membeership tatus: {user_1.is_rewards_member}
-        Rewards card points: {user_1.gold_card_points}
-        '''
-        return user_infos
+        print("==============")
+        print(f"Full name: {self.fname} {self.lname} \nEmail: {self.email} \nAge: {self.age} \nMembeership tatus: {self.is_rewards_member} \nRewards card points: {self.gold_card_points}")
+        print("--------------")
+        return self
     
     def enroll(self):
         if self.is_rewards_member:
             print(f"User already a member.")
-        else:
-            self.is_rewards_member = True
-            self.gold_card_points = 200
+            return self
+        self.is_rewards_member = True
+        self.gold_card_points = 200
+        return self
 
     def spend_points(self, amount):
         if amount < self.gold_card_points:
             self.gold_card_points -= amount
-        else:
-            print(f"Not enough points to spend at this time!")
+            return self
+        print(f"Not enough points to spend at this time!")
+        
 
 
 user_1 = User("Rassim", "Benmhamed", "rassimb@ninja.com", 35)
-print(user_1.display_user_infos())
-user_1.enroll()
-print(user_1.is_rewards_member)
-user_1.spend_points(100)
-print(f"You have {user_1.gold_card_points} points left and available to use.")
+user_2 = User("Sadie", "Flick", "sadief32@ninja.com", 32)
+user_1.display_user_infos().enroll().display_user_infos().spend_points(50).display_user_infos()
+user_2.display_user_infos().enroll().display_user_infos().spend_points(80).display_user_infos()
+
